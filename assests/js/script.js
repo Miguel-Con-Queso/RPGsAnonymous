1d855e5e47e2d3b095685fdd324cef3badec1bba
MiguelConQueso <michael_pnkstn@yahoo.com>
2021-04-23 14:03:32 -0400

    added button js

---
+++ assests/js/script.js
@@ -0,0 +1,35 @@
+function myFunction1() {
+    var x = document.getElementById("so-far");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("cast-members");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("supporters");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("other-projects");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
